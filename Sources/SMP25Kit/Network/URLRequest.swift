fc5a933d0c2a4539d90fe30db8c527f16ea5c535
Jon Gonzalez <jon.gonzalez@mo2o.com>
2025-04-24 20:19:57 +0200

    feat: version 0.1.1

--- Sources/SMP25Kit/Network/URLRequest.swift
+++ Sources/SMP25Kit/Network/URLRequest.swift
@@ -15,17 +15,20 @@ public enum HTTPMethod: String {
 }
 
 extension URLRequest {
-    public static func get(url: URL, method: HTTPMethod = .get) -> URLRequest {
+    public static func get(url: URL, method: HTTPMethod = .get, token: String? = nil) -> URLRequest {
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
         request.timeoutInterval = 5
         request.setValue("application/json", forHTTPHeaderField: "accept")
         request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        if let token {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
 
         return request
     }
-    public static func post<T: Encodable>(url: URL, body: T, method: HTTPMethod = .post) -> URLRequest {
-        var request = URLRequest.get(url: url, method: method)
+    public static func post<T: Encodable>(url: URL, body: T, method: HTTPMethod = .post, token: String? = nil) -> URLRequest {
+        var request = URLRequest.get(url: url, method: method, token: token)
         request.httpBody = try? JSONEncoder().encode(body)
         return request
     }
